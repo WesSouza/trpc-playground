8271f76d5f9a2e00876ad8731f6f4f23f8a1cbc4
Wes Souza <noreply@wes.dev>
2022-08-27 17:06:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# tRPC Playground
+
+A project to explore and have fun with tRPC and Vite.
+
+## License
+
+MIT, https://wes.dev/LICENSE.txt
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+
+import { App } from './App.js';
+
+const appEl = document.querySelector('#app');
+if (appEl) {
+  const root = createRoot(appEl);
+  root.render(<App />);
+}
