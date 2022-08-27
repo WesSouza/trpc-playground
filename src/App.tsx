7e7e9db6f0a34e92ec204f956c4dea8ad9043d13
Wes Souza <noreply@wes.dev>
2022-08-27 18:15:51 -0400

    Add tRPC and Rect Query v3

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { trpc } from './utils/trpc';
+
 export function App() {
-  return <h1>tRPC Playground</h1>;
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      url: '/api/trpc',
+    }),
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <h1>tRPC Playground</h1>
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
 }
